4216db6cd0f575d9c5ae6c5dabfd984008d20c57
vudoanphuongvy <vudoanphuongvy13032007@gmail.com>
2026-04-26 10:44:36 +0700

    Implement main game board and 10x20 grid system

--- TetrisGUI.java
+++ TetrisGUI.java
@@ -28,7 +28,7 @@ public class TetrisGUI extends JFrame {
 
         private void generateStars() {
             stars = new ArrayList<>();
-            for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < 60; i++) {
                 stars.add(new Star(random.nextInt(900), random.nextInt(750), random.nextInt(3) + 1));
             }
         }
@@ -43,25 +43,39 @@ public class TetrisGUI extends JFrame {
             g2d.setPaint(gp);
             g2d.fillRect(0, 0, getWidth(), getHeight());
 
+            g2d.setColor(new Color(255, 255, 255, 20));
+            g2d.setStroke(new BasicStroke(1));
+            for (int i = -500; i < 900; i += 40) {
+                g2d.drawLine(i, 0, i + 750, 750);
+            }
+
+            for (Star star : stars) {
+                g2d.setColor(new Color(255, 255, 255, random.nextInt(100) + 100));
+                g2d.fillOval(star.x, star.y, star.size, star.size);
+            }
+
             g2d.setColor(new Color(255, 255, 255, 15));
             g2d.fillRect(50, 100, 150, 150);
             g2d.fillRect(700, 450, 180, 180);
 
-            for (Star star : stars) {
-                g2d.setColor(new Color(255, 255, 255, 150));
-                g2d.fillOval(star.x, star.y, star.size, star.size);
-            }
+            g2d.setColor(new Color(100, 150, 255));
+            g2d.setStroke(new BasicStroke(4));
+            g2d.drawRect(300, 48, 304, 604);
+            g2d.setColor(new Color(10, 15, 30));
+            g2d.fillRect(302, 50, 300, 600);
 
-            g2d.setColor(new Color(255, 255, 255, 30));
+            g2d.setColor(new Color(40, 45, 70));
             g2d.setStroke(new BasicStroke(1));
-            g2d.drawLine(0, 0, getWidth(), getHeight());
-            g2d.drawLine(0, getHeight() / 4, getWidth(), getHeight() + getHeight() / 4);
-            g2d.drawLine(getWidth() / 4, 0, getWidth() + getWidth() / 4, getHeight());
+            for (int i = 1; i < 10; i++) {
+                g2d.drawLine(302 + i * 30, 50, 302 + i * 30, 650);
+            }
+            for (int j = 1; j < 20; j++) {
+                g2d.drawLine(302, 50 + j * 30, 602, 50 + j * 30);
+            }
         }
 
         private class Star {
             int x, y, size;
-
             Star(int x, int y, int size) {
                 this.x = x;
                 this.y = y;
