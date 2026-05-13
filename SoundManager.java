32c954c0816fbe6a56cf2fe675d7f242986cb6c5
Do Tran Bao Nhu <zkchaeyoung@gmail.com>
2026-05-13 21:53:11 +0700

    implement audio playback system with volume control and sound effects

--- SoundManager.java
+++ SoundManager.java
@@ -27,3 +27,37 @@ public class SoundManager {
             return null;
         }
     }
+    public void playMove() {
+        play(moveClip, -10.0f);
+    }
+    public void playBreak() {
+        play(breakClip, 0.0f);
+    }
+    public void playMusic() {
+        if (musicClip != null) {
+            setVolume(musicClip, -5.0f);
+            musicClip.loop(Clip.LOOP_CONTINUOUSLY);
+            musicClip.start();
+        }
+    }
+    private void play(Clip clip, float volume) {
+        if (clip != null) {
+            setVolume(clip, volume);
+            clip.setFramePosition(0);
+            clip.start();
+        }
+    }
+    private void setVolume(Clip clip, float volume) {
+        try {
+            if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+                FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                float min = gainControl.getMinimum();
+                float max = gainControl.getMaximum();
+                if (volume < min) volume = min;
+                if (volume > max) volume = max;
+                gainControl.setValue(volume);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
